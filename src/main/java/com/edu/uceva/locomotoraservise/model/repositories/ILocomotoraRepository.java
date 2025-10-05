c8ab6982a981659c424fac7cc7f7f7306a13867b
Jhon Alexis Gonzalez Cardena <jhon.gonzalez01@uceva.edu.co>
2025-10-05 00:46:55 -0500

    Ejercicio v0.3.0 completado

---
+++ src/main/java/com/edu/uceva/locomotoraservise/model/repositories/ILocomotoraRepository.java
@@ -0,0 +1,7 @@
+package com.edu.uceva.locomotoraservise.model.repositories;
+
+import com.edu.uceva.locomotoraservise.model.entities.locomotora;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ILocomotoraRepository extends CrudRepository<locomotora, Long> {
+}
