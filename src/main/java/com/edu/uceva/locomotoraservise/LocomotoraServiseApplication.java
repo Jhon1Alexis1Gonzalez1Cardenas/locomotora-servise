a39c1d3423a6acfc86b18a617a6dedd6d7ba59ef
Jhon Alexis Gonzalez Cardena <jhon.gonzalez01@uceva.edu.co>
2025-10-04 23:59:00 -0500

    Commit inicial

---
+++ src/main/java/com/edu/uceva/locomotoraservise/LocomotoraServiseApplication.java
@@ -0,0 +1,13 @@
+package com.edu.uceva.locomotoraservise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocomotoraServiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocomotoraServiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/uceva/locomotoraservise/LocomotoraServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uceva.locomotoraservise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocomotoraServiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
